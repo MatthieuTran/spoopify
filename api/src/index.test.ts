21eb82e1059ed497061940742d04eb9d9e89b156
Matthieu Tran <matthieuktran@gmail.com>
2022-06-12 18:57:44 -0700

    Use mocha

--- api/src/index.test.ts
+++ api/src/index.test.ts
@@ -1,16 +1,21 @@
-import request from "supertest";
-import app from "./app";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import server from "./index";
 
-const mockListen = jest.fn();
-app.listen = mockListen;
+chai.use(chaiHttp);
+chai.should();
 
-afterEach(() => {
-  mockListen.mockReset();
-});
-
-test("Server works", async () => {
-  require("./index");
-
-  expect(mockListen.mock.calls.length).toBe(1);
-  expect(mockListen.mock.calls[0][0]).toBe(process.env.PORT || 3000);
+describe("Koa Server", () => {
+  describe("GET /", () => {
+    it(`should print "Hello World"`, (done) => {
+      chai
+        .request(server)
+        .get("/")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.text.should.equal("Hello World");
+          done();
+        });
+    });
+  });
 });
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,17 @@
-import app from "./app";
+import Koa from "koa";
+import Router from "koa-router";
+
+const app = new Koa();
+const router = new Router();
+
+router.get("/", (ctx: Koa.Context) => {
+  ctx.body = "Hello World";
+});
+
+app.use(router.routes());
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port);
 console.info(`Listening to http://localhost:${port}/`);
+
+export default server;
